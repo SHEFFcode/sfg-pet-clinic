58fd1d50a0e8b9a62d20d17ef797a9b684134b56
SHEFFmachine <sheff@sheffmachine.com>
2018-11-05 20:31:12 -0800

    PC4 Add ID to base entity. Closes #14.

--- pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/BaseEntity.java
@@ -3,7 +3,7 @@ package com.sheffmachine.sfgpetclinic.model;
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
-    private Long id; // use boxed types here, because they can be null, while primitives cannot.
+    private Long id; // use boxed types here, because they can be null, while primitives can not.
 
     public Long getId() {
         return id;
