0cf6a37d66771347ce3423a433746c5e3c978ca7
SHEFFmachine <sheff@sheffmachine.com>
2018-11-08 22:28:47 -0800

    Owners controller

---
+++ pet-clinic-web/src/main/java/com/sheffmachine/sfgpetclinic/controllers/OwnersController.java
@@ -0,0 +1,13 @@
+package com.sheffmachine.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    String listOwners() {
+            return "owners/index";
+    }
+}
