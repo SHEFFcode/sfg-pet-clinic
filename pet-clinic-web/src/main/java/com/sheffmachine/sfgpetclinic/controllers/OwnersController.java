553a18305a0376db5b38b7f76ee365edee96ac43
SHEFFmachine <sheff@sheffmachine.com>
2018-11-08 22:29:20 -0800

    Owners controller. Closes #19

--- pet-clinic-web/src/main/java/com/sheffmachine/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/sheffmachine/sfgpetclinic/controllers/OwnersController.java
@@ -3,7 +3,7 @@ package com.sheffmachine.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/owners")
+@RequestMapping("/owners") // alternative way to do /owners/index etc
 @Controller
 public class OwnersController {
     @RequestMapping({"", "/", "/index", "/index.html"})
