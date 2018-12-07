a447dacd377fd6a2dbfd071b9324b0007e738c44
SHEFFmachine <sheff@sheffmachine.com>
2018-12-06 20:52:25 -0800

    PC-54 Implement base entity.

--- pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,15 @@
 package com.sheffmachine.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // use boxed types here, because they can be null, while primitives can not.
 
     public Long getId() {
