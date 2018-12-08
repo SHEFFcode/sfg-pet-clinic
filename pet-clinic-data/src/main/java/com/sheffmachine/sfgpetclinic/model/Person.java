292c5b5b7b3b00659eefc9fc2566b4c16d6123c6
SHEFFmachine <sheff@sheffmachine.com>
2018-12-07 16:47:29 -0800

    PC-55 Convert owners to JPA entities.

--- pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/model/Person.java
@@ -1,36 +1,15 @@
 package com.sheffmachine.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private Long id;
-    private String address;
-    private String city;
-    private String telephone;
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     @Override
     public Long getId() {
