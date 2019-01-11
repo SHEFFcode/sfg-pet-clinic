7907619658a94f425f267b472e999c54a7344e9d
SHEFFmachine <sheff@sheffmachine.com>
2019-01-10 20:16:23 -0800

    Implement the visit map service

---
+++ pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package com.sheffmachine.sfgpetclinic.services;
+
+import com.sheffmachine.sfgpetclinic.model.Visit;
+import com.sheffmachine.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
