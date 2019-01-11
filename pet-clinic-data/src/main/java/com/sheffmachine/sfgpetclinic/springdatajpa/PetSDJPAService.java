faef696d7a723bab633d1b26a14365f63aa865a9
SHEFFmachine <sheff@sheffmachine.com>
2019-01-10 19:55:15 -0800

    Implement PetSDJPAService. Closes #62

---
+++ pet-clinic-data/src/main/java/com/sheffmachine/sfgpetclinic/springdatajpa/PetSDJPAService.java
@@ -0,0 +1,47 @@
+package com.sheffmachine.sfgpetclinic.springdatajpa;
+
+import com.sheffmachine.sfgpetclinic.model.Pet;
+import com.sheffmachine.sfgpetclinic.repositories.PetRepository;
+import com.sheffmachine.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJPAService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @java.lang.Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
